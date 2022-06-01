304945bb836f513d5a065e5e5cee97fb796feff1
heinrich.danderfer <heinrich.danderfer@connox.de>
2022-06-01 16:12:28 +0200

    child routing

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Outlet } from "react-router-dom";
 import Home from "./routes/home/home.component";
+
+const Navigation = () => (
+  <>
+    <h1>I am the navigation bar</h1>
+    <Outlet />
+  </>
+);
+
+const Shop = () => <h1>I am the shop page</h1>;
 
 const App = () => (
   <Routes>
-    <Route path="/home" element={<Home />} />
+    <Route path="/" element={<Navigation />}>
+      <Route index element={<Home />} />
+      <Route path="shop" element={<Shop />} />
+    </Route>
   </Routes>
 );
 
