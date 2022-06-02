a9f1e6f4db0248d0d98e0b222da9b25dcd1cf946
heinrich.danderfer <heinrich.danderfer@connox.de>
2022-06-02 15:48:50 +0200

    navigation styling

--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -1,10 +1,12 @@
 import { Outlet, Link } from "react-router-dom";
+import "./navigation.styles.scss";
+import { ReactComponent as CrwnLogo } from "../../assets/crown.svg";
 
 const Navigation = () => (
   <>
     <nav>
       <Link className="logo-container" to="/">
-        <div>Logo</div>
+        <CrwnLogo className="logo" />
       </Link>
       <div className="nav-links-container">
         <Link className="nav-link" to="/shop">
